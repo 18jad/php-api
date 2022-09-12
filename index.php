9da6270a4adb728c5efee065ee8688e545e9fdc0
Jad Yahya <jadoyahya@gmail.com>
2022-09-13 02:31:46 +0300

    Created homepage for frontend

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Useless Tools</title>
+</head>
+
+<body>
+    <header>
+        <h1>Useless Tools</h1>
+    </header>
+    <main>
+        <nav>
+            <a href="./palindrome-checker.php">Palindrome Checker</a>
+            <a href="./calculator.php">Calculator</a>
+            <a href="./password-checker.php">Password Checker</a>
+            <a href="./christmas.php">Christmas Timer</a>
+        </nav>
+        <div class="form-section">
+            <h1>Welcome to Useless Tools, here you will find the most useless tools that you will never use.</h1>
+        </div>
+    </main>
+</body>
+
+</html>
